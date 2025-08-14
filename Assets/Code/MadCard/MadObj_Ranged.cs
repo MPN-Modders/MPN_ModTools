0afb6e36aee5906df54895f182eb24ac8b964dec
Michael Swain <thatolswain@gmail.com>
2025-08-14 14:07:38 -0400

    Initial Push

---
+++ Assets/Code/MadCard/MadObj_Ranged.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+
+public class MadObj_Ranged : MadObj_Held
+{
+    [Header("REFS: Ranged")][Space(10)]
+    public GameObject BreachObj;
+
+    public GameObject Point_Shoot;
+    public GameObject Point_Mag;
+    public GameObject Point_Reload;
+    public GameObject Point_Casing;
+
+    public GameObject Addon_Stock;
+    public GameObject Addon_Rail;
+}
